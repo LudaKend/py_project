7425a6e2191a39569c05ed1838f36c9a2cd9ec72
LudaKend <Ludakend@gmail.com>
2023-08-04 18:10:16 +0300

    второй коммит изменений для main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from funcs import udds_up
+from funcs import udds_up, get_difference
 first_term = 1
 second_term = 2
 print(udds_up(first_term, second_term))
+
+print(get_difference(first_term, second_term))
