ca71829f4f9512a09d3ec17f632055d0cf0901c9
LudaKend <Ludakend@gmail.com>
2023-08-04 17:54:06 +0300

    второй коммит изменений в funcs.py

--- funcs.py
+++ funcs.py
@@ -1,4 +1,9 @@
 
-def udds_up (first_term, second_term):
+def udds_up(first_term, second_term):
     '''производит сложение двух чисел'''
     return first_term + second_term
+
+
+def get_difference(minuend, subtrahend):
+    '''производит вычитание'''
+    return minuend - subtrahend
